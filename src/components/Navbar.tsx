9f447a59a15ae05fe12e2f7e507d7b859e944c0e
Akinsanya Joel <akinsanyajoel82@.com>
2024-06-08 13:23:02 +0100

    add product nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import Link from "next/link";
 import { Icons } from "./Icons";
+import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
@@ -14,8 +15,11 @@ const Navbar = () => {
 
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
-                  <Icons.logo className="h-10 w-10 " />
+                  <Icons.logo className="h-12 w-12 " />
                 </Link>
+              </div>
+              <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
+                <NavItems />
               </div>
             </div>
           </div>
---
+++ src/config/index.ts
@@ -0,0 +1,65 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [
+      {
+        name: "Editor choice",
+        href: "#",
+        imageSrc: "/ui-kits/mixed.jpg",
+      },
+      {
+        name: "New Kits",
+        href: "#",
+        imageSrc: "/ui-kits/blue.jpg",
+      },
+      {
+        name: "Bestselling Kits",
+        href: "#",
+        imageSrc: "/ui-kits/purple.jpg",
+      },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks",
+        href: "#",
+        imageSrc: "/icons/picks.jpg",
+      },
+      {
+        name: "New Icons",
+        href: "#",
+        imageSrc: "/icons/new.jpg",
+      },
+      {
+        name: "Bestselling Icons",
+        href: "#",
+        imageSrc: "/icons/bestsellers.jpg",
+      },
+    ],
+  },
+  {
+    label: "Templates",
+    value: "templates" as const,
+    featured: [
+      {
+        name: "Top templates",
+        href: "#",
+        imageSrc: "/templates/top.jpg",
+      },
+      {
+        name: "New templates",
+        href: "#",
+        imageSrc: "/templates/new.png",
+      },
+      {
+        name: "Trending templates",
+        href: "#",
+        imageSrc: "/templates/trending.jpg",
+      },
+    ],
+  },
+];
